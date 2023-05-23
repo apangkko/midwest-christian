c3dc7dc1293d4893ff9057d6bed94a255b79e75f
APANG <apangkko@gmail.com>
2023-05-23 16:06:45 +0700

    submit navbat

--- scripts.js
+++ scripts.js
@@ -1,4 +1,34 @@
-// Swiper JS
+// ---------- Shrink Header ---------- //
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+const header = document.getElementsByTagName("header");
+
+function scrollFunction() {
+  console.log(header);
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header[0].classList.add("sticky");
+  } else {
+    header[0].classList.remove("sticky");
+  }
+}
+
+// ---------- Nav Toggle ---------- //
+const nav = document.querySelector(".nav");
+const toggleBtn = document.querySelector(".toggle");
+
+function toggle() {
+  nav.classList.toggle("active");
+  toggleBtn.classList.toggle("active");
+}
+
+window.addEventListener("resize", (e) => {
+  if(window.innerWidth >= 920) {
+    nav.classList.remove("active");
+    toggleBtn.classList.remove("active");
+  }
+});
+
+// ---------- Swiper JS ---------- //
 var swiper = new Swiper(".testiSlider", {
   loop: true,
   slidesPerView: 1,
